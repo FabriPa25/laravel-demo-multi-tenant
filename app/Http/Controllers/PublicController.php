d9667b6df1bee7ccc3ba688b0c7a59bb9a14b1ce
FabrizioP85 <fabriziopavone9985@gmail.com>
2026-03-04 15:40:03 +0100

    modificatoPublicController

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,6 +6,9 @@ class PublicController extends Controller
 {
     public function index()
     {
+         if (auth()->check()) {
+        return redirect()->route('clients.index');
+    }
         return view('welcome');
     }
 }
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,24 +1,64 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Demo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    
+    <a class="navbar-brand" href="{{ route('welcome') }}">Demo SaaS</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" aria-controls="navbarNav" 
+        aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="{{ route('welcome') }}">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+
+        @auth
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
+          </li>
+        @endauth
+
       </ul>
+
+      <ul class="navbar-nav">
+
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+        @endguest
+
+        @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button"
+               data-bs-toggle="dropdown">
+              {{ Auth::user()->name }}
+            </a>
+
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li>
+                <form method="POST" action="{{ route('logout') }}">
+                  @csrf
+                  <button type="submit" class="dropdown-item">
+                    Logout
+                  </button>
+                </form>
+              </li>
+            </ul>
+          </li>
+        @endauth
+
+      </ul>
+
     </div>
   </div>
 </nav>
