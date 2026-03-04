0cfeed70984774168ef83f329c60ee387fc9aed1
FabrizioP85 <fabriziopavone9985@gmail.com>
2026-03-04 13:49:17 +0100

    fortify

---
+++ resources/views/components/login.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h5 class="text-center">Login</h6>
+        </div>    
+    </div>
+</div>
+
+
+
+</x-layout>
---
+++ resources/views/components/register.balde.php
@@ -0,0 +1,18 @@
+<x-layout>
+  
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="text-center">Register</h1>
+        </div>    
+    </div>
+</div>
+
+
+
+
+
+
+
+</x-layout>
